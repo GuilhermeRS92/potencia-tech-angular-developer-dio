8bd58a81a7a112f481426e722fdcc64dee145eb9
Guilherme Ribeiro de Souza <91293963+GuilhermeRS92@users.noreply.github.com>
2023-05-03 12:36:37 -0300

    integração com a API e limite de pokemons recebidos

--- projeto-pokedex/src/js/main.js
+++ projeto-pokedex/src/js/main.js
@@ -1,24 +1,42 @@
-function convertPokemontToLi(pokemon) {
-    return `
-    <li class="pokemon-box">
-        <h3 class="pokemon-number">#002</h3>
-        <h2 class="pokemon-name">${pokemon.name}</h2>
-        <div class="pokemon-informations">
-            <div class="pokemon-abilities-list">
-                <p class="pokemon-ability">Grass</p>
-                <p class="pokemon-ability">Poison</p>
-            </div>
-            <div class="pokemon-image">
-                <img src="src/images/bulbasaur.webp" alt="${pokemon.name}">
-            </div>
-        </div>
-        <img src="src/images/icon-pokeball-white.svg" class="background-image"></img>
-    </li>
-    `
+const pokemonList = document.getElementById('pokemon-list-container')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const maxRecords = 251
+const limit = 9
+let offset = 0
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+                <li class="pokemon-box ${pokemon.type}">
+                    <h3 class="pokemon-number">#${pokemon.id}</h3>
+                    <h2 class="pokemon-name">${pokemon.name}</h2>
+                    <div class="pokemon-informations">
+                        <div class="pokemon-abilities-list">
+                            ${pokemon.types.map((type) => `<p class="pokemon-ability ${type}">${type}</p>`).join("")}
+                        </div>
+                        <div class="pokemon-image">
+                            <img src="${pokemon.photo}" alt="${pokemon.name}">
+                        </div>
+                    </div>
+                    <img src="src/images/icon-pokeball-white.svg" class="background-image"></img>
+                </li>
+            `).join('')
+        pokemonList.innerHTML += newHtml
+    })
 }
 
-const pokemonList = document.getElementById('pokemon-list-container')
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML = pokemons.map(convertPokemontToLi).join('')
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordNextPage = offset + limit
+
+    if (qtdRecordNextPage >= maxRecords){
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else (
+        loadPokemonItens(offset, limit)
+    )
 })
