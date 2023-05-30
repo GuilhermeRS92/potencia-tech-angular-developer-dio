15043cf8380c52d105f9fc6f7a561bd68a7d522b
Guilherme Ribeiro de Souza <91293963+GuilhermeRS92@users.noreply.github.com>
2023-05-29 22:55:42 -0300

    conexao com json e resultado do quiz

--- projeto-quiz-feed/src/app/components/quiz/quiz.component.ts
+++ projeto-quiz-feed/src/app/components/quiz/quiz.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json";
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit{
+  title: string = ""
 
+  questions: any
+  questionSelected: any
+
+  answers: string[] = []
+  answerSelected:string = ""
+
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+
+  finished: boolean = false;
+
+  constructor(){}
+
+  ngOnInit(): void {
+    if(quiz_questions){
+      this.finished = false;
+      this.title = quiz_questions.title
+
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+      
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChooce(value: string){
+    this.answers.push(value)
+    this.nextStep()
+  }
+
+  async nextStep(){
+    this.questionIndex++
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    } else{
+      const finalAnswer: string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = quiz_questions.results[finalAnswer as keyof typeof quiz_questions.results]
+      console.log(this.answers)
+    }
+  }
+
+  async checkResult(answers: string[]){
+    const result = answers.reduce((prev, curr, i, arr) => {
+      if(
+        arr.filter(item => item === prev).length >
+        arr.filter(item => item === curr).length  //cria um filtro dos itens que são iguais e ve o tamanho do array e verifica se o atual é maior que o anterior
+        ){
+          return prev
+      }else{
+        return curr
+      }
+    })
+
+    return result
+  }
 }
